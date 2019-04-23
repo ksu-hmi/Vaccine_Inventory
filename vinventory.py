a7563ea12ac0cef224290f110cf64b40b8bc1b85
Dominic Thomas <dzt@me.com>
2019-04-23 12:51:04 -0400

    Update vinventory.py
    fixed indentation problems. Fixed while loop to be while True: and fixed logic of db connection test and print out.

--- vinventory.py
+++ vinventory.py
@@ -2,17 +2,6 @@ import sqlite3
 import sys
 import datetime
 
-conn = sqlite3.connect("myinventory.db")
-
-x = 1
-now = datetime.datetime.now()
-
-if (conn) :
-    print ("Database created")
-
-else:
-    print("Error connecting database or connecting to it")
-
 def CreateTable():
     try:
         conn.execute('''CREAT TABLE IF NOT EXSIST VINVENTORY (
@@ -40,28 +29,28 @@ def InsertData():
     except Error as e:
         print (e)
         pass
-    def selectData():
-        try:
-            cursor = conn.execute ("SELECT oid,ProductName, From_,ArrivalDate, ArrivalTime, LocationInInventory FROM VINVENTORY" )
-            print("")
-            print("")
-            print('*********')
-            for row in cursor:
-                print("ID =", row[0])
-                print("ProductName =", row[1])
-                print("From_ =", row[2])
-                print("ArrivalDate =", row[3])
-                print("ArrivalTime=", row[4])
-                print("LocationInInventory =", row[5])
-            print("")
-            print("")
-            print("****Operation Done successfully****")
-        except Error as e:
-            print (e)
-            pass
-        print("Here is your data")
+def selectData():
+    try:
+        cursor = conn.execute ("SELECT oid,ProductName, From_,ArrivalDate, ArrivalTime, LocationInInventory FROM VINVENTORY" )
         print("")
         print("")
+        print('*********')
+        for row in cursor:
+            print("ID =", row[0])
+            print("ProductName =", row[1])
+            print("From_ =", row[2])
+            print("ArrivalDate =", row[3])
+            print("ArrivalTime=", row[4])
+            print("LocationInInventory =", row[5])
+        print("")
+        print("")
+        print("****Operation Done successfully****")
+    except Error as e:
+        print (e)
+        pass
+    print("Here is your data")
+    print("")
+    print("")
 def updateData():
     selectData()
     dte = input("Enter the ID of the column you want to edit")
@@ -91,38 +80,42 @@ def updateData():
         print(e)
         pass
 
-    def deleteData():
-        selectData()
+def deleteData():
+    selectData()
+    id_   =  input("Above is your data choice choose the Id you want to delete:")
+    try:
+        sql = "DELETE FROM VINVENTORY WHERE oid = ?"
+        conn.execute(sql,(str(id_)))
+        conn.commit()
+        print("sucessfully deleted")
+    except Error as e:
+        print (e)
+        pass
 
-        id_   =  input("Above is your data choice choose the Id you want to delete:")
-        try:
-            sql = "DELETE FROM VINVENTORY WHERE oid = ?"
-            conn.execute(sql,(str(id_)))
-            conn.commit()
-            print("sucessfully deleted")
+conn = sqlite3.connect("~\myinventory.db")
+now = datetime.datetime.now()
 
-        except Error as e:
-            print (e)
-            pass
+if (conn) :
+    print ("Connected to database.",conn)
+else:
+    print("Error connecting database or connecting to it")
 
-    while(x):
-        print("Choose the operation to perform")
-        print("press 1 to insert the data")
-        print("press 2 to view the data in database")
-        print("press 3 to update the data in dataset")
-        print("press 4 to Delete the data in dataset")
-        print("press X to exit the system")
-        name = input ("Choose an operation to perform")
-        if (name =="1"):
-            InsertData()
-        elif(name == "2"):
-            SelectData()
-        elif(name == "3"):
-            updateData()
-        elif(name == "4"):
-            deleteData()
-        elif(name=="X"):
-            conn.close()
-            X=0
-
-
+while True:
+    print("Choose the operation to perform")
+    print("press 1 to insert the data")
+    print("press 2 to view the data in database")
+    print("press 3 to update the data in dataset")
+    print("press 4 to Delete the data in dataset")
+    print("press X to exit the system")
+    name = input ("Choose an operation to perform")
+    if (name =="1"):
+        InsertData()
+    elif(name == "2"):
+        SelectData()
+    elif(name == "3"):
+        updateData()
+    elif(name == "4"):
+        deleteData()
+    elif(name=="X"):
+        conn.close()
+        break
